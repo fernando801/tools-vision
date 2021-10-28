2ee7ffbfb3b1525d5d8665eca09a50206401a993
A01769961 <74075416+A01769961@users.noreply.github.com>
2021-10-28 14:33:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import math
 from convolution import convolution
 from sobel import metodo_sobel
+from Simple import metodo_simple
 import urllib.request as urllib
 
 resp = urllib.urlopen("https://drive.google.com/uc?id=1sbsbRr3qWPex9CRM6qMqgzm8MWemJq4i")
@@ -14,5 +15,9 @@ image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 #Metodo Sobel
 #El numero debe de ser un numero impar, mayor a 1 (3/5/7 etc)
 metodo_sobel(image, 5, verbose=True)
+
+#Metodo Simple
+metodo_simple(image,verbose=True)
+
 
 
