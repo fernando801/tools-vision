3cfc8279168d5f8fa7ce174cbd3615ebea9c7e05
rez1619 <92747868+rez1619@users.noreply.github.com>
2021-10-28 13:10:31 -0500

    modificacion del metodo de imagen para URL

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import matplotlib.pyplot as plt
 import math
 from convolution import convolution
 from sobel import metodo_sobel
+import urllib.request as urllib
 
-
-image = cv2.VideoCapture("https://drive.google.com/uc?id=1sbsbRr3qWPex9CRM6qMqgzm8MWemJq4i")
+resp = urllib.urlopen("https://drive.google.com/uc?id=1sbsbRr3qWPex9CRM6qMqgzm8MWemJq4i")
+image = np.asarray(bytearray(resp.read()), dtype="uint8")
+image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 
 #Metodo Sobel
 filterSobel = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
