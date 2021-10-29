ff6d6663433ec2b34630b427eac56998fa0a25be
Fernando Reséndiz <38899746+fernando-resendiz@users.noreply.github.com>
2021-10-28 22:00:55 -0500

    Added comments to gaussian_smoothing.py

--- gaussian_smoothing.py
+++ gaussian_smoothing.py
@@ -4,26 +4,26 @@ from convolution import convolution
 
 #creates kernel
 def gaussian_kernel(K, sigma, verbose=False):
-    M = np.zeros((K,K))
-    mid = K // 2
+    M = np.zeros((K,K))   #creates an empty matrix for the kernel
+    mid = K // 2          #finds the middle of the kernel matrix
     for x in range(K):
         for y in range(K):
-            X = x - mid
-            Y = y - mid
-            M[x][y] = (1/(2*np.pi*sigma**2)) * np.e ** (-(X**2+Y**2)/(2*sigma**2))
+            X = x - mid   #this variables set X and Y
+            Y = y - mid   #relative to the matrix center
+            M[x][y] = (1/(2*np.pi*sigma**2)) * np.e ** (-(X**2+Y**2)/(2*sigma**2)) #calculates kernel values
 
-    if verbose:
+    if verbose:           #plots the kernel if verbose option is enabled 
         plt.imshow(M, interpolation='none',cmap='gray')
         plt.title("Image")
         plt.show()
     
-    return M
+    return M  #return the kernel matrix
 
 #uses convolution with gaussian kernel
 def gaussian_blur(image, kernel_size, sigma, verbose=False):
-    kernel = gaussian_kernel(kernel_size, sigma=sigma, verbose=verbose)
-    conv_img = convolution(image, kernel)
-    if verbose:
+    kernel = gaussian_kernel(kernel_size, sigma=sigma, verbose=verbose) #gets gaussian smoothing kernel
+    conv_img = convolution(image, kernel) #convolutes using gaussian smoothing kernel
+    if verbose: #plots convolution matrix if verbose option is enabled
         plt.imshow(conv_img , interpolation='none',cmap='gray')
         plt.title("Gaussian Blur")
         plt.show()
